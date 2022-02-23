e1fc15c3c80d6d94844eb6f50d3c4f2214c9f2c5
Ahmad Moradi <atrosa@live.com>
2022-02-23 13:49:14 +0330

    Add question 04 solution

---
+++ 04/my-first-async-io.js
@@ -0,0 +1,9 @@
+/* const fs = require('fs');
+
+const path = process.argv[2];
+fs.readFile(path, 'utf-8', (err, result) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(result.split('\n').length - 1);
+}); */
